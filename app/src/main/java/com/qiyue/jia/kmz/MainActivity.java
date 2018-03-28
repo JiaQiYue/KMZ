efb29a399ca47073afe67912788227393f025f94
贾少忠 <shaozhong.jia@hand-china.com>
2018-03-28 15:48:33 +0800

    '基础功能实现'

--- app/src/main/java/com/qiyue/jia/kmz/MainActivity.java
+++ app/src/main/java/com/qiyue/jia/kmz/MainActivity.java
@@ -1,13 +1,69 @@
 package com.qiyue.jia.kmz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView myImageView;
+    private Button myBtnNext;
+    private ArrayList<String> urls;
+    private PictureLoader loader;
+    int picPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loader = new PictureLoader();
+        //初始化数据
+        initData();
+
+        //初始化控件
+        initView();
     }
+
+    private void initData() {
+
+        urls = new ArrayList<>();
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6ipaai7wgj20dw0kugp4.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6gcxc1t7vj20hs0hsgo1.jpg");
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6f5ktcyk0j20u011hacg.jpg");
+        urls.add("http://ww1.sinaimg.cn/large/610dc034jw1f6e1f1qmg3j20u00u0djp.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6aipo68yvj20qo0qoaee.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f69c9e22xjj20u011hjuu.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f689lmaf7qj20u00u00v7.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/c85e4a5cjw1f671i8gt1rj20vy0vydsz.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/610dc034jw1f65f0oqodoj20qo0hntc9.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/c85e4a5cgw1f62hzfvzwwj20hs0qogpo.jpg");
+    }
+
+    private void initView() {
+        myImageView = (ImageView) findViewById(R.id.im_content);
+        myBtnNext = (Button) findViewById(R.id.btn_next);
+        myBtnNext.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_next:
+                //展示下一张图片
+                if (picPosition > 9) {
+                    picPosition = 0 ;
+                }
+                loader.PictureLoader(myImageView, urls.get(picPosition));
+                picPosition++;
+                break;
+            default:
+                break;
+        }
+    }
+
+
 }
